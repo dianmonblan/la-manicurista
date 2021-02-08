4fa0f905100dadb252b555a50ae39d7493920df6
Diego <dianmonbla@gmail.com>
2021-02-08 10:24:12 -0500

    initial commit

---
+++ apps/la-manicurista-nest-api/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'la-manicurista-nest-api',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/la-manicurista-nest-api',
+};
